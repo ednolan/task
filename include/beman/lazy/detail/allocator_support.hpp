ff8cf24ebc074b4745b826c2a2f0ca14d4ad7097
Dietmar Kühl <dietmar.kuehl@me.com>
2025-01-19 23:15:24 +0000

    added a test for allocator_support

--- include/beman/lazy/detail/allocator_support.hpp
+++ include/beman/lazy/detail/allocator_support.hpp
@@ -7,17 +7,35 @@
 #include <beman/lazy/detail/find_allocator.hpp>
 #include <array>
 #include <memory>
-#include <memory_resource>
 #include <new>
 #include <cstddef>
 
 // ----------------------------------------------------------------------------
 
 namespace beman::lazy::detail {
-
+/*!
+ * \brief Utility adding allocator support to type by embedding the allocator
+ * \headerfile beman/lazy/lazy.hpp <beman/lazy/lazy.hpp>
+ *
+ * To add allocator support using this class just publicly inherit from
+ * allocator_support<Allocator, YourPromiseType>. This utility is probably
+ * only useful for coroutine promise types.
+ *
+ * This struct is a massive hack, primarily support allocators for coroutines.
+ * The memory for coroutines is implicitly managed and there isn't a way to
+ * provide the memory directly. Instead, the promise_type can overload an
+ * operator new and somehow determine an allocator based on the arguments
+ * passed to the coroutine. Even worse, the operator delete only gets passed
+ * a pointer to delete and a size. To determine the correct allocator the
+ * operator delete needs to located it based on this information. Putting
+ * the allocator after actually used memory causes the address sanitizer to
+ * object! So, the current strategy is to embed space for the allocator
+ * into the object and pull it out from there.
+ */
 template <typename Allocator, typename Derived>
 struct allocator_support {
     std::array<std::byte, sizeof(Allocator)> buffer;
+    allocator_support() {}
 
     template <typename... A>
     void* operator new(std::size_t size, A&&... a) {
--- tests/beman/lazy/allocator_support.test.cpp
+++ tests/beman/lazy/allocator_support.test.cpp
@@ -2,7 +2,51 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <beman/lazy/detail/allocator_support.hpp>
+#include <memory_resource>
+#include <new>
+#include <cstddef>
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <cassert>
 
 // ----------------------------------------------------------------------------
 
-int main() {}
+namespace {
+struct test_resource : std::pmr::memory_resource {
+    std::size_t outstanding{};
+
+    virtual void* do_allocate(std::size_t size, std::size_t) override {
+        this->outstanding += size;
+        return ::operator new(size);
+    }
+    virtual void do_deallocate(void* ptr, std::size_t size, std::size_t align) override {
+        ::operator delete(ptr, size);
+        this->outstanding -= size;
+    }
+    bool do_is_equal(const memory_resource& other) const noexcept override {
+        auto* tr{dynamic_cast<const test_resource*>(&other)};
+        return tr == this;
+    }
+};
+
+struct some_data {
+    double data{};
+};
+
+template <typename Allocator>
+struct allocator_aware : some_data, beman::lazy::detail::allocator_support<Allocator, allocator_aware<Allocator>> {};
+} // namespace
+
+int main() {
+    delete new allocator_aware<std::pmr::polymorphic_allocator<std::byte>>{};
+    delete new (std::allocator_arg, std::pmr::polymorphic_allocator<std::byte>{})
+        allocator_aware<std::pmr::polymorphic_allocator<std::byte>>{};
+
+    test_resource resource{};
+    assert(resource.outstanding == 0u);
+    auto ptr{new (std::allocator_arg, &resource) allocator_aware<std::pmr::polymorphic_allocator<std::byte>>{}};
+    assert(resource.outstanding != 0u);
+    delete ptr;
+    assert(resource.outstanding == 0u);
+}
