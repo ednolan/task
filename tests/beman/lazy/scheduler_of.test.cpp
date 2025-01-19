442a5bbdb1bb3c69ca059e9055c8088cddb2b1fd
Dietmar Kühl <dietmar.kuehl@me.com>
2025-01-19 14:05:15 +0000

    clang format

--- tests/beman/lazy/scheduler_of.test.cpp
+++ tests/beman/lazy/scheduler_of.test.cpp
@@ -13,21 +13,21 @@
 // ----------------------------------------------------------------------------
 
 namespace {
-    struct no_scheduler {};
+struct no_scheduler {};
 
-    struct defines_scheduler {
-        using scheduler_type = beman::lazy::detail::inline_scheduler;
-    };
+struct defines_scheduler {
+    using scheduler_type = beman::lazy::detail::inline_scheduler;
+};
 
-    struct non_scheduler {};
-    struct defines_non_scheduler {
-        using scheduler_type = non_scheduler;
-    };
-}
+struct non_scheduler {};
+struct defines_non_scheduler {
+    using scheduler_type = non_scheduler;
+};
+} // namespace
 
-int main()
-{
+int main() {
     static_assert(std::same_as<beman::lazy::detail::any_scheduler, beman::lazy::detail::scheduler_of_t<no_scheduler>>);
-    static_assert(std::same_as<beman::lazy::detail::inline_scheduler, beman::lazy::detail::scheduler_of_t<defines_scheduler>>);
-    //using type = beman::lazy::detail::scheduler_of_t<defines_non_scheduler>;
+    static_assert(
+        std::same_as<beman::lazy::detail::inline_scheduler, beman::lazy::detail::scheduler_of_t<defines_scheduler>>);
+    // using type = beman::lazy::detail::scheduler_of_t<defines_non_scheduler>;
 }
